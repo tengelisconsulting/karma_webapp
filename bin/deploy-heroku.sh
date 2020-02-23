a9654ea9429ae38f79a465d971711818efa1c208
Liam Tengelis <ltengelis@gmail.com>
2020-02-23 13:00:15 -0800

    incldue heroku API key

--- bin/deploy-heroku.sh
+++ bin/deploy-heroku.sh
@@ -3,7 +3,9 @@
 IMAGE_NAME="registry.heroku.com/${1}/web"
 IMAGE_ID=$(docker inspect ${IMAGE_NAME} --format={{.Id}})
 PAYLOAD='{"updates":[{"type":"web","docker_image":"'"$IMAGE_ID"'"}]}'
+AUTH_HEADER="Authorization: Bearer ${HEROKU_API_KEY}"
 curl -n -X PATCH https://api.heroku.com/apps/${1}/formation \
      -d ${PAYLOAD} \
      -H "Content-Type: application/json" \
-     -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
+     -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+     -H "${AUTH_HEADER}"
