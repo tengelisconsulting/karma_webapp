286dadd5343bf721a92aa78b8ae39087a43667b6
Liam Tengelis <ltengelis@gmail.com>
2020-02-23 12:54:36 -0800

    release to heroku on success

--- bin/deploy-heroku.sh
+++ bin/deploy-heroku.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-     # - run: $(docker inspect my_image --format={{.Id}})
-
-     # - run: IMAGE_ID="sha256:b7edc14d85404580a3615575489a387dbb7a0f9dd053e84cd62cf6008046f836" \
-     #          && PAYLOAD='{"updates":[{"type":"web","docker_image":"'"$IMAGE_ID"'"}]}' \
-     #          && curl -n -X PATCH https://api.heroku.com/apps/karma-webapp-canary/formation \
-     #          -d ${PAYLOAD} \
-     #          -H "Content-Type: application/json" \
-     #          -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
-
-
-echo "HEY!"
+IMAGE_NAME="registry.heroku.com/${1}/web"
+IMAGE_ID=$(docker inspect ${IMAGE_NAME} --format={{.Id}})
+PAYLOAD='{"updates":[{"type":"web","docker_image":"'"$IMAGE_ID"'"}]}'
+curl -n -X PATCH https://api.heroku.com/apps/${1}/formation \
+     -d ${PAYLOAD} \
+     -H "Content-Type: application/json" \
+     -H "Accept: application/vnd.heroku+json; version=3.docker-releases"
